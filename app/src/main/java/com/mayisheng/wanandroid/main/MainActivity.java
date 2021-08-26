ce6baee5c7808cd617e6e69e62a4733b16c1c63e
mayisheng <13104862372@163.com>
2021-08-26 21:08:19 +0800

    add fragment

--- app/src/main/java/com/mayisheng/wanandroid/main/MainActivity.java
+++ app/src/main/java/com/mayisheng/wanandroid/main/MainActivity.java
@@ -1,12 +1,13 @@
-package com.mayisheng.wanandroid;
+package com.mayisheng.wanandroid.main;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.mayisheng.wanandroid.R;
+
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
